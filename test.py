bae54f81c5acd0eb2c4e76787be7f64129cdc13b
mohamedessameissa <134846004+mohamedessameissa@users.noreply.github.com>
2024-12-26 03:20:22 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@
 
 import streamlit as st
 import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2 import service_account
 
 from datetime import datetime
